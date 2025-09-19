6cc5bad022a314d0e1e9ac3099c7eef57a8a8222
Brianna <briannathagoat@outlook.com>
2025-09-19 12:50:21 -0400

    trying to add favicon

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={poppins.variable}>
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
       </head>
       <body>
